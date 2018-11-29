12fd9fb192b5d513851ed7d4e86209943448442d
ashraf <ashraf1abdelrasool@gmail.com>
2018-11-29 18:03:34 +0200

    push all :fire:

--- src/main/java/org/revo/Config/Processor.java
+++ src/main/java/org/revo/Config/Processor.java
@@ -3,12 +3,13 @@ package org.revo.Config;
 import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 public interface Processor {
     String file_queue = "file_queue";
 
     @Input("file_queue")
-    MessageChannel file_queue();
+    SubscribableChannel file_queue();
 
     String tube_store = "tube_store";
 
