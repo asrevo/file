9d76c64008ed7da63dec7a0fcb484c774e7ffb67
ashraf <ashraf1abdelrasool@gmail.com>
2018-12-04 18:22:02 +0200

    push all :fire:

--- src/main/java/org/revo/Util/FileUtil.java
+++ src/main/java/org/revo/Util/FileUtil.java
@@ -44,7 +44,7 @@ public class FileUtil {
     //    @Override
     public static List<Path> walk(Path stored) {
         List<Path> files = new ArrayList<>();
-        if (is(stored, "avi mkv rmvb mp4")) {
+        if (is(stored, "avi mkv rmvb mp4 flv mov")) {
             files.add(stored);
         } else if (is(stored, "zip rar")) {
             Path unzip = unzip(stored.toFile());
@@ -52,7 +52,7 @@ public class FileUtil {
                 try {
                     Files.walk(unzip)
                             .filter(it -> isRegularFile(it))
-                            .filter(it -> is(it, "avi mkv rmvb mp4"))
+                            .filter(it -> is(it, "avi mkv rmvb mp4 flv mov"))
                             .forEach(files::add);
                 } catch (IOException e) {
                 }
